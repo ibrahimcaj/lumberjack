c25a1145a22f728d731e994d3e034b8d120cbdeb
LightWayUp <lightwayuplightwayup@gmail.com>
2019-06-26 23:15:38 +0800

    Updated chop.js
    - Removed unused module and JSON file requires
    - Reduced hardcoded constants
    - Utilize query string placeholders properly
    - Fixed the cooldown being set even if wood chopping does not succeed
    - Checked array index before accessing element
    - Fixed TypeError thrown if database didn't have any records prior to using the command
    - Removed commented out code for checking axe availability, sticks are free now
    - Fixed the number of wood logs obtained being 0 ~ 4 instead of 1 ~ 5
    - Fixed the number of wood logs in the inventory being the same everytime a tree is chopped
    - Make 5 minute cooldown count between 2 command usages, which is more "correct" from the user perspective
    - ES6 syntax everywhere
    - Added error event listener to db object to stop crash
    - Better formatting, removed space on empty lines

--- commands/chop.js
+++ commands/chop.js
@@ -1,85 +1,61 @@
-const Discord = require("discord.js");
 const sqlite3 = require('sqlite3');
 const random = require('random-value-generator');
 
-const items = require('../items.json');
+const db = new sqlite3.Database('./structures/jack.db').on("error", console.error);
 
-var item = Object.keys(items)[0]
+let cooldown = new Map();
+const cdseconds = 300;
+const codeInputTime = 7;
 
-let db = new sqlite3.Database('./structures/jack.db');
-
-let cooldown = new Set();
-let cdseconds = 300;
-
-module.exports.run = async (client, msg, args) => {
-  if (cooldown.has(msg.author.id)) {
-    return msg.reply("You have to wait 5 minutes between chopping down trees.");
+module.exports.run = async (client, msg) => {
+  if ((msg.createdTimestamp - cooldown.get(msg.author.id)) / 1000 < cdseconds) {
+    return msg.reply(`You have to wait ${cdseconds / 60} minutes between chopping down trees.`);
   } else {
-    cooldown.add(msg.author.id);
-    
-    db.all("SELECT money, inventory FROM wood WHERE id = '" + msg.author.id + "'", function (err, rows) {
-      
-      if (rows[0] === undefined) {
-        db.run(`INSERT INTO wood(id, inventory) VALUES(?, ?)`, [msg.author.id, "{}"], function(err) {
-          //console.log("Inserted.");
-        });
+
+    const idAsNumber = Number(msg.author.id);
+    db.all("SELECT money, inventory FROM wood WHERE id=?", idAsNumber, (err, rows) => {
+
+      if (!rows.length) {
+        rows[0] = {
+          id: idAsNumber,
+          money: 15,
+          inventory: "{}"
+        };
+        db.run("INSERT INTO wood(id, inventory) VALUES(?, ?)", idAsNumber, rows[0].inventory);
       }
-      
-      var obj = JSON.parse(rows[0].inventory);
-      
-      var back = JSON.stringify(obj);
-      
-      /*if (!item[2] in obj || !item[3] in obj || !item[4] in obj || !item[5] in obj || !item[6] in obj) {
-        msg.channel.send("You don't have an axe in your inventory.");
-      } else {*/
-        /*for (i = 6; i > 1; i--) {
-          if (item[i] in obj) {*/
-            msg.channel.send("Searching for the nearest tree to cut down...");
-            
-            const code = random.randomHash(5);
-            
-            const logs = random.randomInteger(5);
-            
-            function chopping(c) {
-              msg.channel.send("I found a tree! To cut the tree down, please enter this code: `" + code + "`. You have 5 seconds.")
-                .then((newmsg) => { //Now newmsg is the message you sent
-                  newmsg.channel.awaitMessages(response => response.content, {
-                    max: 1,
-                    time: 9000,
-                    errors: ['time'],
-                  }).then((collected) => {
-                    if (collected.first().content === code) {
-                      newmsg.channel.send("You have cut the tree down! You got " + logs + " wood logs!");
-                      
-                      /*var obj = JSON.parse(rows.inventory);
-                      
-                      var back = JSON.stringify(obj);*/
-                      
-                      obj["wood"] = logs;
-                      
-                      db.run(`INSERT INTO wood(id, inventory) VALUES(?, ?)`, [msg.author.id, back], function(err) {
-                      });
-                      
-                    } else {
-                      newmsg.channel.send("You entered the wrong code. Please try using the command again.");
-                    }
-                  }).catch(() => {
-                    newmsg.channel.send("You ran out of time, so the woodskeeper kicked you out of the woods. Please try using the command again.");
-                  });
-                });
-            }
-            //break;
-          //}
-        //} 
-      //}
-      
-      setTimeout(chopping, 5000);
+
+      const json = rows[0].inventory;
+      const obj = JSON.parse(json);
+      msg.channel.send("Searching for the nearest tree to cut down...");
+
+      const code = random.randomHash(5);
+
+      const logs = 1 + random.randomInteger(5);
+
+      setTimeout(() => {
+        msg.channel.send(`I found a tree! To cut the tree down, please enter this code: \`${code}\`. You have ${codeInputTime} seconds.`)
+          .then(newmsg => //Now newmsg is the message you sent
+            newmsg.channel.awaitMessages(response => response.content, {
+              max: 1,
+              time: codeInputTime * 1000,
+              errors: ['time'],
+            }).then(collected => {
+              if (collected.first().content === code) {
+                cooldown.set(msg.author.id, msg.createdTimestamp);
+                newmsg.channel.send(`You have cut the tree down! You got ${logs} wood logs!`);
+
+                obj.wood += logs;
+
+                db.run("INSERT INTO wood(id, inventory) VALUES(?, ?)", idAsNumber, JSON.stringify(obj));
+
+              } else {
+                newmsg.channel.send("You entered the wrong code. Please try using the command again.");
+              }
+            }).catch(() =>
+              newmsg.channel.send("You ran out of time, so the woodskeeper kicked you out of the woods. Please try using the command again.")));
+            }, 5000);
     });
   }
-  
-  setTimeout(() => {
-    cooldown.delete(msg.author.id);
-  }, cdseconds * 1000);
 }
 
 module.exports.help = {
