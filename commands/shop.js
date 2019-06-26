c6c061bd3498286329f0cf9a43e9a82ab41eedc1
LightWayUp <lightwayuplightwayup@gmail.com>
2019-06-27 02:09:07 +0800

    Fixed buying items from shop

--- commands/shop.js
+++ commands/shop.js
@@ -7,6 +7,7 @@ const db = new sqlite3.Database('./structures/jack.db').on("error", console.erro
 
 module.exports.run = async (client, msg, args) => {
 
+  const itemlist = Object.keys(items);
   const idAsNumber = Number(msg.author.id);
   db.all("SELECT money, inventory FROM wood WHERE id=?", idAsNumber, (err, rows) => {
 
@@ -29,8 +30,6 @@ module.exports.run = async (client, msg, args) => {
         .setColor('#ff0000')
         .setFooter("Use !shop buy [item] to buy an item from the shop, or use !shop sell [item] to sell and item. | You can also use !shop [item] to look at item info.");
 
-      const itemlist = Object.keys(items);
-
       for (let i = 0; i < itemlist.length; i++) {
         const item = items[itemlist[i]];
         embed.addField(`${item[2]} ${item[0]}`, `Description: ${item[1]}\nPrice: ${item[3]}${item.length === 5 ? `\nDurability: ${item[4]}` : ""}`);
@@ -38,26 +37,27 @@ module.exports.run = async (client, msg, args) => {
 
       msg.channel.send(embed);
     } else if (args[0] === "buy") {
-      for (let i = 0; i < itemlist.length; i++) {
-        if (args.length === 1) {
-          return msg.channel.send("Please specify what you want to buy!");
-        }
+      if (args.length === 1) {
+        return msg.channel.send("Please specify what you want to buy!");
+      }
 
-        if (!itemlist.contains(args[1])) { //this will not work
-          return msg.channel.send("Mate, that item does not exist.");
-        }
+      if (!itemlist.includes(args[1])) {
+        return msg.channel.send("Mate, that item does not exist.");
+      }
+      const item = items[args[1]];
 
-        const difference = rows[0].money - item[3];
-        if (difference >= 0) {
-          db.run(`INSERT INTO wood(id, money) VALUES(?, ?)`, [idAsNumber, difference], err => {
-            if (!err) {
-              console.log("Inserted.");
-            }
-          });
+      const obj = JSON.parse(rows[0].inventory);
+      obj[args[1]] = obj[args[1]] == null ? 1 : obj[args[1]] + 1;
+      const difference = rows[0].money - item[3];
+      if (difference >= 0) {
+        db.run(`INSERT INTO wood(id, money, inventory) VALUES(?, ?, ?)`, [idAsNumber, difference, JSON.stringify(obj)], err => {
+          if (!err) {
+            console.log("Inserted.");
+          }
+        });
 
-        } else {
-          return msg.channel.send("Don't try to scam me mate. You don't have enough money.");
-        }
+      } else {
+        return msg.channel.send("Don't try to scam me mate. You don't have enough money.");
       }
     }
 
