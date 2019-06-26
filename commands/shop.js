cc35e8df195348642f84332f417fee7a8ef1deb8
LightWayUp <lightwayuplightwayup@gmail.com>
2019-06-27 00:30:43 +0800

    Updated shop.js
    - Utilize query string placeholders properly
    - Fixed for loop i not declared first (pls use strict mode in the future)
    - Checked array index before accessing element
    - Fixed TypeError thrown if database didn't have any records prior to using the command
    - Fixed an issue where durability info is never displayed for axes
    - ES6 syntax everywhere
    - Added error event listener to db object to stop crash
    - Better formatting, removed space on empty lines
    - Added missing semicolons
    
    Work in progress!!!

--- commands/shop.js
+++ commands/shop.js
@@ -3,65 +3,67 @@ const sqlite3 = require('sqlite3');
 
 const items = require('../items.json');
 
-let db = new sqlite3.Database('./structures/jack.db');
+const db = new sqlite3.Database('./structures/jack.db').on("error", console.error);
 
 module.exports.run = async (client, msg, args) => {
-  
-  db.all("SELECT money, inventory FROM wood WHERE id = '" + msg.author.id + "'", function (err, rows) {
-    
-    if (rows[0] === undefined) {
-      db.run(`INSERT INTO wood(id, inventory) VALUES(?, ?)`, [msg.author.id, "{}"], function(err) {
-        console.log("Inserted.");
+
+  const idAsNumber = Number(msg.author.id);
+  db.all("SELECT money, inventory FROM wood WHERE id=?", idAsNumber, (err, rows) => {
+
+    if (!rows.length) {
+      rows[0] = {
+        id: idAsNumber,
+        money: 15,
+        inventory: "{}"
+      };
+      db.run("INSERT INTO wood(id, inventory) VALUES(?, ?)", [idAsNumber, rows[0].inventory], err => {
+        if (!err) {
+          console.log("Inserted.");
+        }
       });
     }
-    
-    if (!args[0]) {
-		var embed = new Discord.RichEmbed()
+
+    if (!args.length) {
+		  const embed = new Discord.RichEmbed()
         .setAuthor("Shop")
         .setColor('#ff0000')
         .setFooter("Use !shop buy [item] to buy an item from the shop, or use !shop sell [item] to sell and item. | You can also use !shop [item] to look at item info.");
-        
-        var itemlist = Object.keys(items)
-        
-        for (i = 0; i < itemlist.length; i++) {
-          var item = items[itemlist[i]]
-          
-          if (itemlist[i].includes("Axe")) {
-          	embed.addField(item[2] + " " + item[0], "Description: " + item[1] + "\nPrice: " + item[3] + "\nDurability: " + item[4]);
-          }
-          
-          embed.addField(item[2] + " " + item[0], "Description: " + item[1] + "\nPrice: " + item[3]);
+
+        const itemlist = Object.keys(items);
+
+        for (let i = 0; i < itemlist.length; i++) {
+          const item = items[itemlist[i]];
+          embed.addField(`${item[2]} ${item[0]}`, `Description: ${item[1]}\nPrice: ${item[3]}${item.length === 5 ? `\nDurability: ${item[4]}` : ""}`);
         }
-        
+
        msg.channel.send(embed);
     } else if (args[0] === "buy") {
-		for (i = 0; i < itemlist.length; i++) {
-			if (!args[1]) {
-				return msg.channel.send("Please specify what you want to buy!");
-			}
-			
-			if (!itemlist.contains(args[1])) { //this will not work
-				return msg.channel.send("Mate, that item does not exist.");
-			}
-			
-			if (rows[0].money >= item[3]) {
-				var moneycalc = rows[0].money - item[3]
-				
-				db.run(`INSERT INTO wood(id, money) VALUES(?, ?)`, [msg.author.id, moneycalc], function(err) {
-					console.log("Inserted.");
-				});
-				
-				
-			} else {
-				return msg.channel.send("Don't try to scam me mate. You don't have enough money.");
-			}
-		}
+		  for (let i = 0; i < itemlist.length; i++) {
+			  if (args.length === 1) {
+				  return msg.channel.send("Please specify what you want to buy!");
+			  }
+
+			  if (!itemlist.contains(args[1])) { //this will not work
+				  return msg.channel.send("Mate, that item does not exist.");
+			  }
+
+        const difference = rows[0].money - item[3];
+			  if (difference >= 0) {
+				  db.run(`INSERT INTO wood(id, money) VALUES(?, ?)`, [idAsNumber, difference], err => {
+            if (!err) {
+              console.log("Inserted.");
+            }
+				  });
+
+			  } else {
+				  return msg.channel.send("Don't try to scam me mate. You don't have enough money.");
+			  }
+		  }
     }
-    
+
   });
-}
+};
 
 module.exports.help = {
   name: "shop"
-}
-
+};
