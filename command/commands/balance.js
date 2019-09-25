ibrahimcaj/lumberjack

commit e5c7bf2578f0093240564ef6569c0c31a9dc464b

Fixed UserResolver incorrectly discarding partial source
- Fixed an issue where UserResolver was unconditionally discarding characters after the 37th in the source string, even if a word would be split as a result. Now it discards after (and including) the first space after the 37th character

utility/discord/UserResolver.js
@@ -126,7 +126,7 @@ class UserResolver {
         if (!(t(source, "string") && t(client, Discord.Client))) {
             throw new TypeError("Incorrect type(s) for resolveUser arguments!");
         }
-        source = source.substring(0, 32 + "#0000".length);
+        source = source.substring(0, source.indexOf(" ", 32 + "#0000".length));
 
         const matchedUsers = new Set();
         return new Promise(async (resolve, reject) => {

commit 49f3e1e520e7efe9241e3250df97072902e48bac

Removed Singleton restriction of UserResolver
- Made UserResolver design no longer Singleton, instead a "default" UserResolver instance is set as property of client object
- Fixed a bug introduced with e5c7bf2, where source strings that don't contain space would be truncated to an empty string, thus failing to resolve to any User
- Added conveconvenience method `newOption` to UserResolver, eliminating the need for separated UserResolver.js import

command/commands/balance.js
@@ -27,7 +27,6 @@ const {retrieve} = require("../../Database.js");
 const {Emotes} = require("../../Constants.js");
 const Discord = require("discord.js");
 const MessageUtilities = require("../../utility/discord/MessageUtilities.js");
-const UserResolver = require("../../utility/discord/UserResolver.js");
 const {
     t,
     unwrap
@@ -54,8 +53,8 @@ async function run(client, message, args) {
     let discordUser;
     if (args.length) {
 
-        const mentionedUsers = await UserResolver.getInstance()
-            .resolveUser(content.substring(content.indexOf(args[0])), client, new UserResolver.Option({recursive: true}));
+        const mentionedUsers = await client.userResolver
+            .resolveUser(content.substring(content.indexOf(args[0])), client, client.userResolver.newOption({recursive: true}));
         const mentionedNonBots = mentionedUsers.filter(user => !user.bot);
         const nonBotsLength = mentionedNonBots.length;
 

command/commands/fixdata.js
@@ -25,7 +25,6 @@
 
 const {fixData} = require("../../Database.js");
 const MessageUtilities = require("../../utility/discord/MessageUtilities.js");
-const UserResolver = require("../../utility/discord/UserResolver.js");
 const Discord = require("discord.js");
 const Help = require("../Help.js");
 const {isDeveloper} = require("../../Config.js");
@@ -59,8 +58,8 @@ async function run(client, message, args) {
     let id;
     if (args.length) {
 
-        const mentionedUsers = await UserResolver.getInstance()
-            .resolveUser(content.substring(content.indexOf(args[0])), client, new UserResolver.Option({recursive: true}));
+        const mentionedUsers = await client.userResolver
+            .resolveUser(content.substring(content.indexOf(args[0])), client, client.userResolver.newOption({recursive: true}));
         const mentionedNonBots = mentionedUsers.filter(user => !user.bot);
         const nonBotsLength = mentionedNonBots.length;
 

command/commands/inventory.js
@@ -28,7 +28,6 @@ const {getAllTypes} = require("../../utility/ItemUtilities.js");
 const {Emotes} = require("../../Constants.js");
 const Discord = require("discord.js");
 const MessageUtilities = require("../../utility/discord/MessageUtilities.js");
-const UserResolver = require("../../utility/discord/UserResolver.js");
 const {
     t,
     unwrap
@@ -55,8 +54,8 @@ async function run(client, message, args) {
     let discordUser;
     if (args.length) {
 
-        const mentionedUsers = await UserResolver.getInstance()
-            .resolveUser(content.substring(content.indexOf(args[0])), client, new UserResolver.Option({recursive: true}));
+        const mentionedUsers = await client.userResolver
+            .resolveUser(content.substring(content.indexOf(args[0])), client, client.userResolver.newOption({recursive: true}));
         const mentionedNonBots = mentionedUsers.filter(user => !user.bot);
         const nonBotsLength = mentionedNonBots.length;
 

index.js
@@ -101,7 +101,8 @@ client.once(Events.READY, () => {
     changeActivity();
     client.setInterval(changeActivity, 15000);
 
-    client.guilds.filter(guild => guild.memberCount >= 5000).forEach(guild => UserResolver.getInstance().addToBlacklist(guild));
+    client.userResolver = new UserResolver();
+    client.guilds.filter(guild => guild.memberCount >= 5000).forEach(guild => client.userResolver.addToBlacklist(guild));
 
 }).on(Events.MESSAGE_CREATE, async message => {
 

utility/discord/UserResolver.js
@@ -29,32 +29,20 @@ const {
 } = require("../Type.js");
 const Discord = require("discord.js");
 
-const privateValidator = Symbol();
-let instance;
-
 class UserResolver {
 
-    constructor(_privateValidator) {
-
-        if (_privateValidator !== privateValidator) {
-            throw new Error("UserResolver is private!");
-        }
+    constructor() {
 
         this.guildBlacklist = [];
         this.userBlacklist = [];
     }
 
-    static getInstance() {
-
-        if (!instance) {
-            instance = new UserResolver(privateValidator);
-        }
-
-        return instance;
-    }
-
     static get Option() {
         return UserResolverOption;
+    }
+
+    newOption(object) {
+        return new UserResolverOption(object);
     }
 
     addToBlacklist(target) {
@@ -126,7 +114,10 @@ class UserResolver {
         if (!(t(source, "string") && t(client, Discord.Client))) {
             throw new TypeError("Incorrect type(s) for resolveUser arguments!");
         }
-        source = source.substring(0, source.indexOf(" ", 32 + "#0000".length));
+        const splitIndex = source.indexOf(" ", 32 + "#0000".length);
+        if (splitIndex !== -1) {
+            source = source.substring(0, splitIndex);
+        }
 
         const matchedUsers = new Set();
         return new Promise(async (resolve, reject) => {